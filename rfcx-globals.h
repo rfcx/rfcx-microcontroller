866428ae60cdf98a79bfefac26c4b1d9c48e0878
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-07-22 23:37:51 -0400

    Adding global definitions file for F_CPU to use with delays

---
+++ rfcx-globals.h
@@ -0,0 +1,17 @@
+/**********************************************************
+*	RFCx Microcontroller Software - Global Definitions
+*
+*	Kalee Stutzman 	(stutzmak@mail.gvsu.edu)
+*	Joe Gibson		(gibsjose@mail.gvsu.edu)
+*
+*	08 July 2015
+*
+*   www.rfcx.org
+**********************************************************/
+
+#ifndef RFCX_GLOBALS_H
+#define RFCX_GLOBALS_H
+
+#define F_CPU 8000000UL     //Clock Speed (Hz)
+
+#endif//RFCX_GLOBALS_H
